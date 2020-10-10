36654566b76a3b3460db2f861f9bf0f2f0ec93ed
oshanielikewela <oshani.17@business.mrt.ac.lk>
2020-10-10 12:10:05 +0530

    pythonapp-v1.1

---
+++ pythonApp.py
@@ -0,0 +1,10 @@
+name=raw_input("enter ur name: ")
+gender=raw_input("are u F/M: ")
+status=raw_input("married/unmarried: ")
+if gender=="F" or "f" or "female" or "Female" or "FEMALE":
+    if status=="unmarried" or "Unmarried" or "UNMARRIED":
+        print "Miss.",name
+    else:
+        print "Mrs,",name
+else:
+    print "Mr.",name
